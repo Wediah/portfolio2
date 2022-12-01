bb0a257847c6a7136ad8d1eb463040db043e2c2b
Wediah <emmanuelwediah@gmail.com>
2022-12-01 23:19:27 +0000

    card system update

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -9,16 +9,16 @@ function Landing () {
     return (
         <Element id="home" name="home">
             <div className="min-h-screen px-10 " >
-                <h1 className="pt-60 md:pt-80 text-6xl font-bold pb-4">
+                <h1 className="pt-60 md:pt-80 text-4xl font-bold pb-4 md:text-6xl">
                     Hi, there! <br />
                     I'm Emmanuel Wediah, a <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 animate-out slide-out-to-right duration-300 delay-150">UI/UX designer</span> and a <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 ">Front-end Engineer</span>. <br />
                 </h1>
-                <h2 className="font-bold text-slate-500 pb-4">
+                <h2 className="font-bold text-slate-500 pb-4 text-md md:text-xl">
                     I'm passionate about helping brands bring imaginations to life right from the capital of Ghana, Accra. <br />
                 </h2>
                 <button type="button" title="button" className="bg-cyan-500 p-3 rounded-xl font-bold text-white animate-pulse md:hidden">Download my resume</button>
 
-                <h4 className="pt-16 md:pt-32 text-blue-300 font-bold text-sm animate-bounce font-mono">
+                <h4 className="pt-16 md:pt-32 md:text-lg text-blue-300 font-bold text-sm animate-bounce font-mono">
                     there's more scroll down
                 </h4>
             </div>
--- pages/about.tsx
+++ pages/about.tsx
@@ -9,33 +9,27 @@ function About () {
     return (
         <Element id="about" name="about">
             <div className="min-h-screen px-10 " >
-                <h4 className="">Services I offer;</h4>
-                <div className="shadow-2xl rounded-2xl text-center p-10 my-10">
-                    <h3 className="pt-8 pb-2 text-2xl font-bold">UX Design</h3>
-                    <p className="py-2">I love to create result-oriented user experience. My primary goal is to create simple but rich, engaging and innovative user experiences.</p>
-                    <h4 className="text-cyan-500 py-4 font-bold">Design tools I use; </h4>
-                    <p className="text-gray-800 py-1">Adobe XD</p>
-                    <p className="text-gray-800 py-1">Adobe Photoshop</p>
-                    <p className="text-gray-800 py-1">Figma</p>
-                </div>
-                <div className="shadow-2xl rounded-2xl text-center p-10 my-10">
-                    <h3 className="pt-8 pb-2 text-2xl font-bold">Front-end Development</h3>
-                    <p className="py-2">I'm a meticulus web developer with 6 months of experience in front-end development and passion to build responsive websites which are user freindly.</p>
-                    <h4 className="text-cyan-500 py-4 font-bold">Languages and frameworks I use;</h4>
-                    <p className="text-gray-800 py-1">HTML, CSS, JavaScript, TypeScript,</p>
-                    <p className="text-gray-800 py-1">React, Nextjs, Bootstrap, Tailwindcss & JQuery</p>
-                    
-                    <h4 className="text-cyan-500 py-4 font-bold">Design tools I use; </h4>
-                    <p className="text-gray-800 py-1">Visual Studio Code</p>
-                    
-                </div>
-                <div className="shadow-2xl rounded-2xl text-center p-10 my-10">
-                    <h3 className="pt-8 pb-2 text-2xl font-bold"></h3>
-                    <p className="py-2">I love to create result-oriented user experience. My primary goal is to create simple but rich, engaging and innovative user experiences.</p>
-                    <h4 className="text-cyan-500 py-4 font-bold">Design tools I use; </h4>
-                    <p className="text-gray-800 py-1">Adobe XD</p>
-                    <p className="text-gray-800 py-1">Adobe Phontoshop</p>
-                    <p className="text-gray-800 py-1">Figma</p>
+                <h4 className="text-4xl font-bold pt-40">Services I offer;</h4>
+                <div className=" gap-10 lg:flex pt-10">
+                    <div className="shadow-2xl rounded-2xl text-center p-10 my-10">
+                        <h3 className="pt-8 pb-2 text-2xl font-bold">UX Design</h3>
+                        <p className="py-2">I love to create result-oriented user experience. My primary goal is to create simple but rich, engaging and innovative user experiences.</p>
+                        <h4 className="text-cyan-500 py-4 font-bold">Design tools I use; </h4>
+                        <p className="text-gray-800 py-1">Adobe XD</p>
+                        <p className="text-gray-800 py-1">Adobe Photoshop</p>
+                        <p className="text-gray-800 py-1">Figma</p>
+                    </div>
+                    <div className="shadow-2xl rounded-2xl text-center p-10 my-10">
+                        <h3 className="pt-8 pb-2 text-2xl font-bold">Front-end Development</h3>
+                        <p className="py-2">I'm a meticulus web developer with 6 months of experience in front-end development and passion to build responsive websites which are user freindly.</p>
+                        <h4 className="text-cyan-500 py-4 font-bold">Languages and frameworks I use;</h4>
+                        <p className="text-gray-800 py-1">HTML, CSS, JavaScript, TypeScript,</p>
+                        <p className="text-gray-800 py-1">React, Nextjs, Bootstrap, Tailwindcss & JQuery</p>
+                        
+                        <h4 className="text-cyan-500 py-4 font-bold">Design tools I use; </h4>
+                        <p className="text-gray-800 py-1">Visual Studio Code</p>
+                        
+                    </div>
                 </div>
             </div>
         </Element>
