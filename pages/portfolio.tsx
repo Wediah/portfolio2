e86ad1870208a22305b83ecbe60b7940a2c29c68
Wediah <emmanuelwediah@gmail.com>
2022-12-01 23:30:20 +0000

    portfolio page added

---
+++ pages/portfolio.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Transition } from '@headlessui/react'
+import { Link } from "react-scroll"
+import Image from "next/image";
+import { Element } from 'react-scroll'
+
+function Portfolio () {
+    return (
+        <Element id="portfolio" name="portfolio">
+
+        </Element>
+    );
+}
+
+export default Portfolio;
