0b6766490d426279f21c85ae33a766bf15772e7c
Wediah <emmanuelwediah@gmail.com>
2022-12-04 22:59:55 +0000

    images added to public

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -6,6 +6,7 @@ import { Element } from 'react-scroll'
 import inter1 from '../public/inter1.png'
 import inter2 from '../public/inter 2.png'
 import inter3 from '../public/08th Nov.png'
+import {AiOutlineArrowRight} from 'react-icons/ai'
 
 import eng from '../public/eng.png'
 
@@ -14,20 +15,32 @@ function Portfolio () {
         <Element id="portfolio" name="portfolio">
             
             <div className="min-h-screen px-10">
-             <h4 className="text-4xl font-bold pt-40">Portifolio</h4>
+             <h4 className="text-4xl font-bold pt-40 text-center">Portifolio</h4>
+             <h3 className="text-md font-light  text-center">What I have done</h3>
                 <div className="flex flex-col py-10 gap-10 lg:flex-row lg:flex-wrap">
                     <div className="basis-1/3 flex-1">
                         <Image alt="" src={inter1} className="rounded-lg object-cover" width={"100"}  height={'100'} layout="responsive"/>
+                        <h3 className="font-medium text-2xl">UX/UI Design</h3>
+                        <h3 className="font-medium text-md">Modern Website</h3>
                     </div>
                     <div className="basis-1/3 flex-1">
                         <Image alt=""  src={inter2} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive"/>
+
+                        <h3 className="font-medium text-2xl">UX/UI Design</h3>
+                        <h3 className="font-medium text-md">Modern Website</h3>
                     </div>
                     <div className="basis-1/3 flex-1">
                         <Image alt=""  src={inter3} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive"/>
+                        
+                        <h3 className="font-medium text-2xl">UX/UI Design</h3>
+                        <h3 className="font-medium text-md">Ecommerce shop</h3>
                     </div>
                     <div className="basis-1/3 flex-1 ">
                      <Image alt=""  src={eng} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive"/>
-                     
+
+                     <h3 className="font-medium text-2xl">Web</h3>
+                     <h3 className="font-medium text-md">Modern Website</h3>
+                     <h3 className="flex text-sm mt-4">View website <AiOutlineArrowRight className="text-xl ml-2"/> </h3>
                     </div>
                 </div>
                 
