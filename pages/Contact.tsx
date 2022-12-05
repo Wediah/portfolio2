d8bc496fd5b2817bbd0f30de9f651406b5b2dbdc
Wediah <emmanuelwediah@gmail.com>
2022-12-05 20:35:30 +0000

    download button added

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -64,7 +64,7 @@ function Contact () {
                     <a href="https://twitter.com/WediahE"><FiTwitter className="hover:transition hover:-translate-y-2 duration-300 delay-150"/></a>
                     <a href="https://wa.me/message/DGI7R7QWOUTAJ1"><AiOutlineWhatsApp className="hover:transition hover:-translate-y-2 duration-300 delay-150"/></a>
                 </div>
-                <div className="text-center pb-5 font-medium text-md">© Copyright Wediah Emmanuel. All rights reserved</div>
+                <div className="text-center pb-5 font-medium text-md">2022 © Wediah Emmanuel. All rights reserved.</div>
 
             </div>
         </Element>
--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -3,22 +3,39 @@ import { Transition } from '@headlessui/react'
 import { Link } from "react-scroll"
 import Image from "next/image";
 import { Element } from 'react-scroll'
-
+import { AnimationOnScroll } from 'react-animation-on-scroll'
+import 'animate.css'
 
 
 
 function Landing () {
+
+    const onButtonClick = () => {
+        fetch('WediahEmmanuelresume.docx').then(response => {
+            response.blob().then(blob => {
+
+                const fileURL = window.URL.createObjectURL(blob);
+
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'WediahEmmanuelresume.docx';
+                alink.click();
+            })
+        })
+    }
+
     return (
         <Element id="home" name="home">
             <div className="min-h-screen px-10 " >
                 <h1 className="pt-60 md:pt-80 text-4xl font-bold pb-4 md:text-6xl">
                     Hi, there! <br />
-                    I'm Emmanuel Wediah, a <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 animate-out slide-out-to-right duration-300 delay-150">UI/UX designer</span> and a <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 ">Front-end Engineer</span>. <br />
+                    I'm Emmanuel Wediah, a <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 animate_animated animate_bounce ">UI/UX designer</span> and a <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 ">Front-end Engineer</span>. <br />
                 </h1>
                 <h2 className="font-bold text-slate-500 pb-4 text-md md:text-xl">
                     I'm passionate about helping brands bring imaginations to life right from the capital of Ghana, Accra. <br />
                 </h2>
-                <button type="button" title="button" className="bg-cyan-500 p-3 rounded-xl font-bold text-white animate-pulse md:hidden">Download my resume</button>
+
+                <button type="button" onClick={onButtonClick} className="bg-cyan-500 p-3 rounded-xl font-bold text-white md:hidden">Download my resume</button>
 
                 <h4 className="pt-16 md:pt-32 md:text-lg text-blue-300 font-bold text-sm animate-bounce font-mono">
                     there's more scroll down
