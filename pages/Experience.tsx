ab5fc92346d952592d249ef06d2344433f7fd67f
Wediah <emmanuelwediah@gmail.com>
2022-12-05 01:54:42 +0000

    cards added

--- pages/Experience.tsx
+++ pages/Experience.tsx
@@ -15,17 +15,19 @@ function Experience () {
                 <h3 className="text-md font-light  text-center">What I have participated in</h3>
                 <div className=" gap-10 lg:flex pt-10">
                     <div className="shadow-2xl rounded-3xl text-center p-10 my-10 flex-1 bg-gray-100">
-                        <h4 className="text-2xl font-semibold">Front-end Developer - Intern at <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">CSIR-INSTI</span></h4>
-                        <h3 className="text-md font-bold text-cyan-400 pt-2">Oct 2022 - Nov 2022</h3>
+                        <h4 className="text-2xl font-semibold">Front-end Developer - <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">Freelance</span></h4>
+                        <h3 className="text-md font-bold text-cyan-400 pt-2">April 2022 - Present</h3>
                         <h3 className="text-sm font-semibold pt-4">Responsibility</h3>
-                        <h5 className="flex text-md pt-1"> <GoPrimitiveDot className="text-xl "/>Maintaining and redesigning code and interfaces respectively. </h5>
+                        <h5 className="flex text-md pt-1"> <GoPrimitiveDot className="text-xl text-center "/>Developing websites for brands. </h5>
+                        <h5 className="flex text-md pt-1"> <GoPrimitiveDot className="text-xl text-center "/>Updating and implementing features in website. </h5>
                     </div>
                     <div className="shadow-2xl rounded-3xl text-center p-10 my-10 flex-1 bg-gray-100">
-                    <h4 className="text-2xl font-semibold">Front-end Developer - Intern at <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">CSIR-INSTI</span></h4>
+                    <h4 className="text-2xl font-semibold">Front-end Developer - Intern at <a href="http://insti.csir.org.gh/"><span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">CSIR-INSTI</span></a></h4>
                         <h3 className="text-md font-bold text-cyan-400 pt-2">Oct 2022 - Nov 2022</h3>
                         <h3 className="text-sm font-semibold pt-4">Responsibility</h3>
                         <h5 className="flex text-md pt-1"> <GoPrimitiveDot className="text-xl text-center"/>Maintaining code. </h5>
                         <h5 className="flex text-md pt-1"> <GoPrimitiveDot className="text-xl text-center"/>Designing and redesigning interfaces. </h5>
+                        <h5 className="flex text-md pt-1"> <GoPrimitiveDot className="text-xl text-center"/>Testing and debugging bugs in website. </h5>
                     </div>
                 </div>
 
