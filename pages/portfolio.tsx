259562faf324ed3f14d38fdb5d6026db165b9e27
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-09 10:56:36 +0000

    adds links to ad portal

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -5,6 +5,7 @@ import inter1 from '../public/inter1.png'
 import inter2 from '../public/inter 2.png'
 import inter3 from '../public/08th Nov.png'
 import tour from '../public/tour.png'
+import ad from '../public/ad.png'
 import {AiOutlineArrowRight} from 'react-icons/ai'
 
 import eng from '../public/eng.png'
@@ -49,11 +50,11 @@ function Portfolio () {
                      <a href="https://tourismpage.vercel.app/" ><button className="group/edit flex text-sm mt-4 bg-gray-100 p-2 rounded-full dark:bg-gray-800 ">Live Demo<AiOutlineArrowRight className="text-xl ml-2 group-hover/edit:translate-x-2.5"/> </button></a>
                     </div>
                     <div className="basis-1/3 flex-1 ">
-                     <Image alt=""  src={tour} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive"/>
+                     <Image alt=""  src={ad} className="rounded-lg object-cover" width={'100'} height={'100'} layout="responsive"/>
 
                      <h3 className="font-medium text-2xl">Web</h3>
                      <h3 className="font-medium text-md dark:text-gray-200">Modern Website</h3>
-                     <a href="https://tourismpage.vercel.app/" ><button className="group/edit flex text-sm mt-4 bg-gray-100 p-2 rounded-full dark:bg-gray-800 ">Live Demo<AiOutlineArrowRight className="text-xl ml-2 group-hover/edit:translate-x-2.5"/> </button></a>
+                     <a href="https://ad-eight.vercel.app/" ><button className="group/edit flex text-sm mt-4 bg-gray-100 p-2 rounded-full dark:bg-gray-800 ">Live Demo<AiOutlineArrowRight className="text-xl ml-2 group-hover/edit:translate-x-2.5"/> </button></a>
                     </div>
                 </div>
                 
