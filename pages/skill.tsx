13965c747d9821ae4a98a604c279f31d7d656ce5
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-09 11:27:11 +0000

    reduces gap in skills

--- pages/skill.tsx
+++ pages/skill.tsx
@@ -30,11 +30,11 @@ function Skill() {
             <div className="in-h-screen px-10 dark:text-white">
             <h4 className="text-4xl font-bold pt-40 text-center">Skills</h4>
             <h3 className="text-md font-light  text-center ">What I can do</h3>
-            <div className="gap-10 lg:flex">
+            <div className="gap-5 lg:flex">
                 <div  className="p-10  my-10 lg:w-3/5">
                     <div className="flex justify-center  text-md font-bold pb-5"> <HiCodeBracket className="text-2xl mr-2"/>   
                     Frontend Development</div>
-                    <div className="flex justify-center flex-wrap gap-10 mt-5 lg:flex-row lg:flex-wrap">
+                    <div className="flex justify-center flex-wrap gap-5 mt-5 lg:flex-row lg:flex-wrap">
                        <div className="text-center dark:bg-gray-800 bg-gray-100 p-10 rounded-full group/edit"> 
                         <Image src={html} width={50} alt=""  className="pb-2 hover:transition group-hover/edit:-translate-y-4 duration-300 delay-150 mx-auto"/> 
                         <span className="text-md font-serif font-bold">HTML</span> <br />
@@ -63,7 +63,7 @@ function Skill() {
                 <div className="p-10 my-10 lg:w-3/5">
                     <div className="flex justify-center text-md font-bold pb-5"> <MdWeb className="text-2xl mr-2"/>   
                     UX Design</div>
-                    <div className="flex justify-center flex-wrap gap-10 mt-5 lg:flex-row lg:flex-wrap">
+                    <div className="flex justify-center flex-wrap gap-5 mt-5 lg:flex-row lg:flex-wrap">
                        <div className="text-center dark:bg-gray-800 bg-gray-100 p-10 rounded-full group/edit"> <Image src={figma} width={50} alt="" className="pb-2 hover:transition group-hover/edit:-translate-y-4 duration-300 delay-150 mx-auto"/><span className="text-md font-serif font-bold">Figma</span> <br />
                         <span className="text-sm text-gray-500">Basic</span>  </div>
                        <div className="text-center dark:bg-gray-800 bg-gray-100 p-10 rounded-full group/edit"> <Image src={xd} width={50} alt="" className="pb-8 hover:transition group-hover/edit:-translate-y-4 duration-300 delay-150 mx-auto"/><span className="text-md font-serif font-bold">Adobe XD</span> <br />
